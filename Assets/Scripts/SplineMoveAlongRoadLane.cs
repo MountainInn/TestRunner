8ff44bcc0a9614de1a34c5d146d43d1cbd1f76ba
Mountain <blckclst@gmail.com>
2023-05-12 23:22:00 +0500

    Simplify SplineMoveAlongRoadLane

--- Assets/Scripts/SplineMoveAlongRoadLane.cs
+++ Assets/Scripts/SplineMoveAlongRoadLane.cs
@@ -1,17 +1,14 @@
 using UnityEngine;
 
-public class SplineMoveAlongRoadLane : SplineMove
+public class SplineMoveAlongRoadLane : MonoBehaviour
 {
-    [SerializeField, HideInInspector]
-    SplineRoadLanes roadLanes => TargetSpline.GetComponent<SplineRoadLanes>();
+    [SerializeField] private SplineRoadLanes roadLanes;
 
     private int lane = 0;
     private Vector3 laneOffset, targetLaneOffset;
-    private bool IsSwitchingLane => laneOffset != targetLaneOffset;
 
-    protected override void Start()
+    private void Start()
     {
-        base.Start();
         lane = roadLanes.Mid;
     }
 
@@ -27,33 +24,14 @@ public class SplineMoveAlongRoadLane : SplineMove
         lane = Mathf.Clamp(lane, 0, roadLanes.LaneCount-1);
     }
 
-    protected override void UpdateTransform()
+    private void Update()
     {
-        if (targetSpline == null)
-        {
-            Debug.LogError("Target Spline is null");
-            return;
-        }
-
-        float t = UpdatePathRatio();
-
-        EvaluatePositionAndRotation(t, out var position, out var rotation);
-
-        UpdateLaneOffset(ref position, rotation);
-
-        transform.position = position;
-        transform.rotation = rotation;
-    }
-
-    private void UpdateLaneOffset(ref Vector3 position, in Quaternion rotation)
-    {
-        targetLaneOffset = roadLanes.GetLaneOffset(lane);
-
         float t = Time.fixedDeltaTime * 10;
+
+        targetLaneOffset = roadLanes.GetLaneOffset(lane);
 
         laneOffset = Vector3.Lerp(laneOffset, targetLaneOffset, t);
 
-        position += rotation * laneOffset;
+        transform.localPosition = laneOffset;
     }
-
 }
